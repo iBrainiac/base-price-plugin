40b699cf4498947f40a3621443c323e80044beec
ibrainiac <franqhazard@gmail.com>
2024-09-22 01:56:29 +0300

    creating a plugin for data price feeds on base network

--- testing.mjs
+++
@@ -1,9 +0,0 @@
-import { Web3 } from "web3";
-import Chronicle from "./index.mjs";
-
-const web3 = new Web3("https://gateway.tenderly.co/public/sepolia");
-web3.registerPlugin(new Chronicle());
-
-web3.basedprice.getbaseEthPrice();
-web3.basedprice.getMaticPrice();
-web3.basedprice.getcbBTCPrice();
